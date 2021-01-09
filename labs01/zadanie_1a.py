1f99887767411edbe83d2c620cc68df1d0d23730
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2021-01-09 07:32:02 +0100

    labs 01

---
+++ labs01/zadanie_1a.py
@@ -0,0 +1,8 @@
+
+"""
+ * stwórz zmienną o nazwie `pi` i o wartości 3.14.
+ * stwórz zmienną o nazwie `promien` i o wartości 12.
+ * oblicz pole koła i przypisz wynik do zmniennej `pole`.
+ * wyświetl wynik na ekran.
+""" 
+
